7f90349b066ca8b36275ba1053621a1bdcd9816a
Dabani <robertmaku@yahoo.fr>
2020-10-27 08:07:40 +0200

    Setup app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
@@ -17,6 +20,8 @@ import { BoardManagerComponent } from './board-manager/board-manager.component';
 import { BoardAgentComponent } from './board-agent/board-agent.component';
 import { BoardPlayerComponent } from './board-player/board-player.component';
 import { BoardUserComponent } from './board-user/board-user.component';
+
+import { authInterceptorProviders } from './_helpers/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -37,9 +42,11 @@ import { BoardUserComponent } from './board-user/board-user.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
